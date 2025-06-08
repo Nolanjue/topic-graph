f565d126223fb2569c513aa392ed388accdeb8f2
Milo Guan <mguan2@scu.edu>
2025-06-08 14:38:22 -0700

    updated utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -15,9 +15,6 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-nltk.download('punkt')
-nltk.download('stopwords')
-
 def extract_text_from_pdf(file):
     maxnumpages = 100
     reader = PyPDF2.PdfReader(file)
@@ -30,34 +27,33 @@ def extract_text_from_pdf(file):
             break
     return text
 
+#chunks the text into chunks of roughly 300 words
 def chunk_text(text, chunk_size=300):
-    sentences = re.split(r'(?<=[.!?])\s+', text)
+    filtered_text = remove_stop_words(text)
+    words = filtered_text.split()
     chunks, current = [], ""
 
-    for sentence in sentences:
-        filtered_sentence = remove_stop_words(sentence)
-        if len(current) + len(filtered_sentence) < chunk_size:
-            current += " " + filtered_sentence
+    wordcount = 0
+    for word in words:
+        if(wordcount < chunk_size):
+            current += ' ' + word
+            wordcount += 1
         else:
             chunks.append(current.strip())
-            current = filtered_sentence
-    if current:
+            current = word
+            wordcount = 1
+
+    #the cutoff of the final chunk is 100, since a very small chunk could mess up LDA
+    if wordcount > 100:
         chunks.append(current.strip())
 
-    # Preprocess each chunk (inside this function as you suggested!)
-    processed_chunks = []
-    for chunk in chunks:
-        tokens = word_tokenize(chunk.lower())
-        tokens = [t for t in tokens if t.isalpha()]# and t not in STOPWORDS
-        processed_chunks.append(" ".join(tokens))  # back to string for vectorizer
-
-    return chunks, processed_chunks
+    return chunks
 
 def remove_stop_words(text):
     words = text.split()
-    STOPWORDS = set(nltk.stopwords("english"))
-    filtered_words = [word for word in words if word.lower() not in STOPWORDS]
-
+    STOPWORDS = set(stopwords.words("english"))
+    filtered_words = [word.lower() for word in words if word.lower() not in STOPWORDS and word.isalpha()]#remove all english stopwords, and all non-alphabetic characters
+    #also sets all words to lowercase
     filtered_text = ' '.join(filtered_words)
 
     return filtered_text
